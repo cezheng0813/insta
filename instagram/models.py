7763d07c03ff05c9e168fdbc2931e74d60a234da
cezheng0813 <cezheng@buffalo.edu>
2020-01-30 16:40:08 -0500

    db/superuser

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.TextField(blank = True, null= True)
+    image = models.ImageField(upload_to= 'static/images/posts',
+    blank = True,
+    null = True)
+    
+    def __str__(self):
+        return self.title
